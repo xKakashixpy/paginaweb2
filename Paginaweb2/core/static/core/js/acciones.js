047824b3846fb22b62182f041f554cc475b272ef
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-04 07:22:35 -0400

    Incorporacion Carrito de Compras

--- Paginaweb2/core/static/core/js/acciones.js
+++ Paginaweb2/core/static/core/js/acciones.js
@@ -9,3 +9,20 @@ imagenes.forEach((imagen) => {
         imagen.classList.remove('resaltado');
     });
 });
+
+
+// Obtener el elemento del carrito
+var cartElement = document.getElementById("cart-icon");
+
+// Variable para llevar el conteo de los elementos en el carrito
+var cartCount = 0;
+
+// Función para agregar un elemento al carrito
+function addToCart() {
+  cartCount++;
+  cartElement.textContent = "Carrito (" + cartCount + ")";
+}
+
+// Asociar la función addToCart a algún evento, por ejemplo, al hacer clic en un botón "Agregar al carrito"
+var addToCartButton = document.getElementById("add-to-cart-button");
+addToCartButton.addEventListener("click", addToCart);

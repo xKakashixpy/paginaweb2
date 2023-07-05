96e23f613395a5f9fdf8e6e8540ad3b9ef324b31
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-04 23:42:29 -0400

    carrito funcional

--- Paginaweb2/core/static/core/js/acciones.js
+++ Paginaweb2/core/static/core/js/acciones.js
@@ -1,28 +1,9 @@
-const imagenes = document.querySelectorAll('.image-block img');
-
-imagenes.forEach((imagen) => {
-    imagen.addEventListener('mouseover', () => {
-        imagen.classList.add('resaltado');
-    });
-
-    imagen.addEventListener('mouseout', () => {
-        imagen.classList.remove('resaltado');
-    });
-});
-
-
-// Obtener el elemento del carrito
-var cartElement = document.getElementById("cart-icon");
-
-// Variable para llevar el conteo de los elementos en el carrito
-var cartCount = 0;
-
-// Función para agregar un elemento al carrito
-function addToCart() {
-  cartCount++;
-  cartElement.textContent = "Carrito (" + cartCount + ")";
-}
-
-// Asociar la función addToCart a algún evento, por ejemplo, al hacer clic en un botón "Agregar al carrito"
-var addToCartButton = document.getElementById("add-to-cart-button");
-addToCartButton.addEventListener("click", addToCart);
+function moveCard(card) {
+    card.style.transform = 'translateY(-5px)';
+    card.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+  }
+  
+  function resetCard(card) {
+    card.style.transform = 'none';
+    card.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+  }

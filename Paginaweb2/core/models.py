cfad07241d0dcd97e88da4f779be99f10aec1755
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-06 02:27:55 -0400

    aplicacion de todas las tablas nuevas, se restauro git

--- Paginaweb2/core/models.py
+++ Paginaweb2/core/models.py
@@ -1,9 +1,59 @@
 from django.db import models
 
 class Producto(models.Model):
+    codigo = models.CharField(max_length=10)
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
     descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    mensaje = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    producto = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.producto
+    
+class Inventario(models.Model):
+    producto = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.producto
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=10)
 
     def __str__(self):
         return self.nombre

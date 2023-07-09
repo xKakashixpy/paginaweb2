f68397b42c97ff8240b56b22f2aea01c5161b3d9
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-09 12:30:38 -0400

    creacion tabla categorias y aplicada al modelo producto

--- Paginaweb2/core/models.py
+++ Paginaweb2/core/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Producto(models.Model):
     codigo = models.CharField(max_length=10)
     nombre = models.CharField(max_length=100)
-    precio = models.IntegerField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
     descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.nombre} ->{self.precio}'
+    

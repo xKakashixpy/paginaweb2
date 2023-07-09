6e4009cc180af53a5ec8f8f4520cf338ed8cd65f
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-08 22:56:10 -0400

    Carrito Recibiendo datos de la BD

--- Paginaweb2/core/urls.py
+++ Paginaweb2/core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from.views import home, tienda, misionyvision, servicios, noticias,carrito
 
--- Paginaweb2/core/views.py
+++ Paginaweb2/core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Producto
+from core.Carrito import Carrito
+
 
 # Create your views here.
 
@@ -6,7 +9,32 @@ def home(request):
     return render(request,'core/home.html')
 
 def tienda(request):
-    return render(request, 'core/tienda.html')
+    productos = Producto.objects.all()
+    return render(request, 'core/tienda.html', {'productos': productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect('tienda')
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect('tienda')
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect('tienda')
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect('tienda')
+
 
 def misionyvision(request):
     return render(request, 'core/misionyvision.html')

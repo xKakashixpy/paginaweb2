ff5c03c6b05cb34007e3827c4080865652e8034d
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-07-11 13:34:13 -0400

    Incio de Session

--- Paginaweb2/core/urls.py
+++ Paginaweb2/core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home, tienda, misionyvision, servicios, noticias, agregar_producto, eliminar_producto, restar_producto, limpiar_carrito, producto_crud, registrarproducto, eliminarproducto,editarproducto, editarproducto2,categoria_crud, registrarcategoria,editarcategoria,editarcategoria2,eliminarcategoria, cliente_crud, registrarcliente, editarcliente, editarcliente2,eliminarcliente, contacto_crud,editarcontacto, editarcontacto2,eliminarcontacto,registrarcontacto, proveedor_crud,registrarproveedor,editarproveedor2, editarproveedor, eliminarproveedor
 
 
@@ -37,5 +37,6 @@ urlpatterns = [
     path('registrarproveedor/', registrarproveedor, name="registrarproveedor"),
     path('editarproveedor/<str:rut>/',editarproveedor, name='editarproveedor'),
     path('editarproveedor2/<str:rut>/', editarproveedor2,name="editarproveedor2"),
-    path('eliminarproveedor/<str:rut>/', eliminarproveedor, name="eliminarproveedor" )  
+    path('eliminarproveedor/<str:rut>/', eliminarproveedor, name="eliminarproveedor" ),  
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
